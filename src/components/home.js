0e64262f1f0810e76346daaa14d4ad62cc5ac55d
Justen Quirante <jcquirante@gmail.com>
2019-01-23 12:21:40 -0800

    Completed project setup

---
+++ src/components/home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default props => (
+    <div className="center">
+        <h1>Welcome to Chat App</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum at quidem nesciunt delectus officia doloribus alias similique cupiditate, repudiandae autem numquam, sequi quibusdam, cumque fuga.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum at quidem nesciunt delectus officia doloribus alias similique cupiditate, repudiandae autem numquam, sequi quibusdam, cumque fuga.</p>
+    </div>
+)
